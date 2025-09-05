baa1abecd6e81f44e7d71ddd206d260339027f41
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2025-09-05 10:20:42 -0500

    touched up the read and setuser funcs

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/hectoribarra2024-eng/blog_aggregator/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello go!")
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("hector")
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
 }
